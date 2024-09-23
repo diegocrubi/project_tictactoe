885cfe2143952a0505d0ed8ed161cebc2875e90c
Diego <diegocrubi@gmail.com>
2024-09-22 20:27:45 -0500

    add UI object

--- script.js
+++ script.js
@@ -24,49 +24,158 @@
 
 */
 
-const player = (name, token) => {
-  const getName = name;
-  const getToken = token;
+function gameBoard() {
+  const makeBoard = () => {
+    return Array.from(Array(9));
+  };
+
+  let board = makeBoard();
+
+  const getBoard = () => board;
+
+  const updateBoard = (index, token) => {
+    if (board[index] != null) {
+      //return "Hello!"
+    }
+
+    if (board[index] == null) {
+      board[index] = token;
+    }
+
+    return board;
+  };
+
+  /* Can't quite figure this out. As of now it won't reassign board
+which means I'm misunderstanding something about scoping
+9/2/2024: What I was actually misunderstanding was reassigning a value
+for some reason, tryign to reassign within the function and using 
+let or const before board didn't let me change the actual value
+*/
+  const resetBoard = () => {
+    board = makeBoard();
+  };
 
   return {
-    getName,
-    getToken,
+    makeBoard,
+    getBoard,
+    updateBoard,
+    resetBoard,
   };
-};
+}
 
-const Gameboard = (function(){
+function cellLogic() {
+  // set a default value to each cell
+  let value = null;
 
-    function
+  const addToken = (player) => {
+    value = player;
+  };
+
+  const getValue = () => value;
+
+  return {
+    addToken,
+    getValue,
+  };
+}
+
+// what information do I need about a player for the purposes of playing tic tac toe
+// What is the game flow process
+
+// game controller handles the flow of the game
+function gameController(playerOne = "Player One", playerTwo = "Player Two") {
+  /*
+  - the game is initialized
+    - create game object
+  - the first player is initialized
+    - create the player object 
+  - the first player picks a square
+   - handled by the game object
+  - the new state of the game is shown
+  - the players are switched, and now you have a new active player
+  - I guess all of the above would be considered a round, but there are probably other ways of conceptualizing the game
+  what should it return? 
+  */
+
+  const players = {
+    playerOne: {
+      playerOneName: playerOne,
+      token: "X",
+    },
+    playerTwo: {
+      playerTwoName: playerTwo,
+      token: "Y",
+    },
+  };
+  const game = gameBoard();
+  let currentPlayer = players.playerOne;
+
+  const switchPlayer = () => {
+    if (currentPlayer == players.playerOne) {
+      currentPlayer = players.playerTwo;
+    } else {
+      currentPlayer = players.playerOne;
+    }
+
+    return currentPlayer;
+  };
+
+  const getCurrentPlayer = () => currentPlayer;
+
+  /* Playing a round involves picking a spot, 
+  switching the player, 
+  printing the board
+  */ 
+  const playRound = (index, token = getCurrentPlayer().token) => {
 
 
-
-})(); 
-
-const game = (function(){
-
+    game.updateBoard(index, token = token)
     
+    switchPlayer()
 
-})(); 
+  }
 
-const createUI = (function () {
-  function createGameboard() {
-    let gameboard = document.querySelector(".gameboard");
-    for (i = 1; i < 10; i++) {
-      let square = document.createElement("div");
+  return {
+    switchPlayer,
+    getCurrentPlayer,
+    playRound, 
+    game
+
+  };
+}
+
+function gameInterface() {
+  const game = gameController();
+  const scoreboard = document.querySelector('.scoreboard');
+  const boardDiv = document.querySelector(".gameboard");
+
+  const updateScreen = () => {
+    // Clear the existing board
+    boardDiv.innerHTML = '';
+
+    const board = game.game.getBoard();
+    
+    for (let i = 0; i < board.length; i++) {
+      let square = document.createElement("button");
       square.classList.add("square");
-      square.setAttribute("id", i);
-      square.innerText = i;
-      gameboard.appendChild(square);
+      square.dataset.index = i;
+      square.innerText = board[i] || ''; // Use empty string if the cell is null
+      boardDiv.appendChild(square);
     }
   }
 
-  // What all do we want back?
-  return {
-    createGameboard,
-  };
-})();
+  function buttonPress() {
+    boardDiv.addEventListener('click', (e) => {
+      if (e.target.classList.contains('square')) {
+        const selectedButton = e.target.dataset.index;
+        game.playRound(selectedButton);
+        updateScreen();
+      }
+    });
+  }
 
-// run functions
+  updateScreen();
+  buttonPress();
+}
 
-// obviously the first thing is we gotta create the UI
-createUI.createGameboard();
+gameInterface();
